913a6d977c8b86fa4fbd86d310edfd812f26ed66
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-08-01 19:43:20 +0200

    Send magic number and service port number as part of beacon payload

--- src/listen.rs
+++ src/listen.rs
@@ -2,32 +2,27 @@ use simpdiscoverylib::BeaconListener;
 use simplog::simplog::SimpleLogger;
 use std::time::Duration;
 
-const BEACON_PORT : u16 = 9001;
+const BEACON_TEST_SERVICE_NAME :&str = "BeaconTestService";
 
 fn main() -> std::io::Result<()> {
     SimpleLogger::init_prefix(Some("info"), false);
 
     let args : Vec<String> = std::env::args().collect();
-    let filter = match args.len() {
-        0..=1 => None,
-        _ => Some(args[1].clone())
+    let service_name = match args.len() {
+        0..=1 => BEACON_TEST_SERVICE_NAME.to_string(),
+        _ => args[1].clone()
     };
 
     let timeout = match args.len() {
-        0..=2 => None,
-        _ => {
-            Some(Duration::from_secs(args[2].parse::<u64>().unwrap()))
-        }
+        1 => None,
+        _ => Some(Duration::from_secs(args[2].parse::<u64>().unwrap()))
     };
 
-    println!("Waiting for a beacon");
-    if let Some(time) = timeout {
-        println!("Timeout set to {} seconds", time.as_secs_f64());
-    }
+    println!("Timeout set to {:?}", timeout);
 
-    if let Ok(listener) = BeaconListener::new(BEACON_PORT, filter.map(|f| f.into_bytes())) {
-        println!("Beacon {}", listener.wait(timeout)?);
-    }
+    println!("Waiting for a beacon from service: '{}'", service_name);
+    let listener = BeaconListener::new(service_name.as_bytes())?;
+    println!("Beacon {}", listener.wait(timeout)?);
 
     Ok(())
 }
